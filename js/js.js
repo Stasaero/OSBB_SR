334a9fd5674446b6ea8b346363db23efc29fa24d
Stanislav Karpenko <Karp24111988@gmail.com>
2022-04-24 11:29:15 +0200

    updated time in js

--- js/js.js
+++ js/js.js
@@ -3,7 +3,7 @@ setInterval(change_background, 1000 * 60 * 60);
 function change_background() {
   let currentDate = new Date();
   let time = currentDate.getHours()
-    if (time >= 20 || time < 7) {
+    if (time >= 20 || time < 12) {
    
  document.getElementById("mainphoto").style.backgroundImage = "url('./pictures/mainphoto.jpeg')";
   }
