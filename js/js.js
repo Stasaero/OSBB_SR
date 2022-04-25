ed44f351b181586df307cf8fc291079582f47b50
Stanislav Karpenko <Karp24111988@gmail.com>
2022-04-25 13:23:29 +0200

    changes to js.js
    document.body.innerHTML = document.body.innerHTML.replace('bg-light', 'bg-dark');
          document.body.innerHTML = document.body.innerHTML.replace('navbar-light', 'navbar-dark');

--- js/js.js
+++ js/js.js
@@ -4,28 +4,22 @@ function change_background() {
   let currentDate = new Date();
   let time = currentDate.getHours()
     if (time >= 20 || time < 7) {
-   
- var obj = document.getElementById("mainphoto");
- obj.setAttribute("style", "background-image: url('./pictures/mainphoto.jpeg'); background-position: 8%; ");
-  }
+      var obj = document.getElementById("mainphoto");
+      obj.setAttribute("style", "background-image: url('./pictures/mainphoto.jpeg'); background-position: 8%; ");
+      document.body.innerHTML = document.body.innerHTML.replace('bg-light', 'bg-dark');
+      document.body.innerHTML = document.body.innerHTML.replace('navbar-light', 'navbar-dark');
+      
+     }
  else {
     document.getElementById("mainphoto").style.backgroundImage = "url('./pictures/mainphoto2.jpeg')";
     
   } 
 }
-    
 
-    
-    
-// $(document).ready(function() {
-//     $("#cf7_controls").on('click', 'span', function() {
-//       $("#cf7 img").removeClass("opaque");
+
+
   
-//       var newImage = $(this).index();
-  
-//       $("#cf7 img").eq(newImage).addClass("opaque");
-  
-//       $("#cf7_controls span").removeClass("selected");
-//       $(this).addClass("selected");
-//     });
-//   });
+ 
+ 
+ 
+
