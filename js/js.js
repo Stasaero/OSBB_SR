e859ea5c53830b36b653fd0e931146cd5eb657e7
Stanislav Karpenko <Karp24111988@gmail.com>
2022-04-28 23:00:46 +0200

    added docs and background color function

--- js/js.js
+++ js/js.js
@@ -3,7 +3,7 @@ setInterval(change_background, 1000 * 60 * 60);
 function change_background() {
   let currentDate = new Date();
   let time = currentDate.getHours()
-    if (time >= 20 || time < 7) {
+    if (time >= 23 || time < 7) {
       var obj = document.getElementById("mainphoto");
       obj.setAttribute("style", "background-image: url('./pictures/mainphoto.jpeg'); background-position: 8% 1%; ");
       document.body.innerHTML = document.body.innerHTML.replace('bg-light', 'bg-dark');
@@ -26,8 +26,14 @@ document.body.innerHTML = document.body.innerHTML.replace('bg-light', 'bg-dark')
 document.body.innerHTML = document.body.innerHTML.replace('navbar-light', 'navbar-dark');
 }
 else {
-   
-   
+     
  } 
 }
 
+function backgroundcolor () {
+  let currentDate = new Date();
+  let time = currentDate.getHours()
+    if (time >= 20 || time < 7) {
+document.getElementById("docs_body").style.backgroundColor = "#495057"; 
+}
+}
